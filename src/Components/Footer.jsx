ce8f87da8981a1763d8dcd6fc9834351ab246924
skghosh87 <sarghosh@gmail.com>
2025-10-07 23:51:38 +0600

    Footer Added Demo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import Container from "./Container";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer Section</h1>
-    </div>
+    <footer className="bg-neutral text-neutral-content p-10">
+      <Container>
+        <div className="footer grid grid-cols-2 md:grid-cols-5 justify-between">
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </div>
+        <div className="text-left md:text-center text-gray-500 text-sm mt-8">
+          Copyright © 2025 - All right reserved
+          <br className="block md:hidden" /> All rights reserved.
+        </div>
+      </Container>
+    </footer>
   );
 };
 
